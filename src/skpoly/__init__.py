c40e5649de569c8a1bcf3eeba70463e73208c80c
Alex Shtof <alex.shtf@gmail.com>
2025-09-26 22:03:52 +0300

    Add linting and type checking tooling

--- src/skpoly/__init__.py
+++ src/skpoly/__init__.py
@@ -1,8 +1,7 @@
 """Polynomial basis transformers compatible with scikit-learn."""
 
-from ._legendre import LegendreFeatures
-
 from ._bernstein import BernsteinFeatures
+from ._legendre import LegendreFeatures
 
 __all__ = [
     "BernsteinFeatures",
